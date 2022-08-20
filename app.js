30859f9a12677415d0586d32c8c6d4b688cb5760
andresguastavino <andresguastavino@gmail.com>
2022-08-20 01:13:09 -0300

    Added 404 response for not defined routes

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(router);
+app.use((req, res, next) => {
+    res.status(404).json({ result: 'error', errors: [ 'Requested route not found' ]});
+});
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${ PORT } and in ${ NODE_ENV } mode`);
