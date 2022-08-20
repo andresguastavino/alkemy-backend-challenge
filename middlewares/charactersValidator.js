69ff10f8560828035e51f8c785028774c149192d
andresguastavino <andresguastavino@gmail.com>
2022-08-20 01:14:44 -0300

    Modified and finished charactersRouter and charactersValidtor

--- middlewares/charactersValidator.js
+++ middlewares/charactersValidator.js
@@ -5,54 +5,256 @@ const {
     validateIsNumber, 
     validateMinValue,
     validateDecimalPlace, 
-    validateIsPositive 
+    validateIsPositive,
+    validIsInteger,
+    validateIsArray,
+    validateStringIsNumber
 } = require('../utils/validations');
 
+const { Character, MovieOrSerie, CharacterMovieOrSerie } = require('../database');
+const { Op } = require('sequelize');
+
+const methods = {
+    Post: 'Post',
+    Put: 'Put',
+    Delete: 'Delete'
+};
+
 const validations = {
+    characterId : [
+        async (characterId, errors, method) => {
+            if(method !== methods.Put && method !== methods.Delete) return;
+
+            const currentErrors = errors.length;
+            validateNotNull(characterId, errors, 'CharacterId needs to be specified');
+            if(errors.length > currentErrors) return;
+            validateStringIsNumber(characterId, errors, 'CharacterId must be a number');
+            if(errors.length > currentErrors) return;
+
+            const character = await Character.findByPk(characterId);
+            console.log(character);
+            if(character === null) {
+                errors.push(`There is no Character with id ${ characterId }`);
+            }
+        },
+    ],
     name: [
-        (name, errors) => validateNotNull(name, errors, 'Character\'s Name is mandatory'),
-        (name, errors) => validateType(name, 'string', errors, 'Name must be a string'),
-        (name, errors) => validateMaxLength(name, 50, false, errors, 'Name\'s length cannot be more than 50 characters')
+        (name, errors, method) => { method === methods.Post && validateNotNull(name, errors, 'Character\'s Name is mandatory'); },
+        (name, errors) => { validateType(name, 'string', errors, 'Name must be a string'); },
+        (name, errors) => { validateMaxLength(name, 50, false, errors, 'Name\'s length cannot be more than 50 characters'); }
     ],
     age: [
-        (age, errors) => validateNotNull(age, errors, 'Character\'s Age is mandatory'),
-        (age, errors) => validateMinValue(age, 0, true, errors, 'Age must be greater than 0'),
-        (age, errors) => validateIsPositive(age, errors, 'Age must be a positive number')
+        (age, errors, method) => { method === methods.Post && validateNotNull(age, errors, 'Character\'s Age is mandatory'); },
+        (age, errors) => { validateMinValue(age, 0, true, errors, 'Age must be greater than 0'); },
+        (age, errors) => { validateIsPositive(age, errors, 'Age must be a positive number'); }
     ],
     weight: [
-        (weight, errors) => validateNotNull(weight, errors, 'Character\'s Weight is mandatory'),
-        (weight, errors) => validateIsNumber(weight, errors, 'Weight must be a number'),
-        (weight, errors) => validateDecimalPlace(weight, 2, errors, 'The Weight can only have 2 decimal places at most')
+        (weight, errors, method) => { method === methods.Post && validateNotNull(weight, errors, 'Character\'s Weight is mandatory'); },
+        (weight, errors) => { validateIsNumber(weight, errors, 'Weight must be a number'); },
+        (weight, errors) => { validateDecimalPlace(weight, 2, errors, 'The Weight can only have 2 decimal places at most'); }
     ],
     history: [
-        (history, errors) => validateNotNull(history, errors, 'Character\'s History is mandatory'),
-        (history, errors) => validateType(history, 'string', errors, 'History must be a string'),
-        (history, errors) => validateMaxLength(history, 500, false, errors, 'History\'s length cannot be more than 500 characters')
+        (history, errors, method) => { method === methods.Post && validateNotNull(history, errors, 'Character\'s History is mandatory'); },
+        (history, errors) => { validateType(history, 'string', errors, 'History must be a string'); },
+        (history, errors) => { validateMaxLength(history, 500, false, errors, 'History\'s length cannot be more than 500 characters'); }
     ],
     image: [
-        (image, errors) => validateNotNull(image, errors, 'Character\'s Image is mandatory'),
-        (image, errors) => validateType(image, 'string', errors, 'Image must be a string'),
-        (image, errors) => validateMaxLength(image, 250, false, errors, 'Image\'s length cannot be more than 250 characters')
+        (image, errors, method) => { method === methods.Post && validateNotNull(image, errors, 'Character\'s Image is mandatory'); },
+        (image, errors) => { validateType(image, 'string', errors, 'Image must be a string'); },
+        (image, errors) => { validateMaxLength(image, 250, false, errors, 'Image\'s length cannot be more than 250 characters'); }
+    ],
+    moviesOrSeriesIds: [
+        async (moviesOrSeriesIds, errors, method) => {
+            const validation = (moviesOrSeriesIds, errors) => {
+                const currentErrors = errors.length;
+                validateIsArray(moviesOrSeriesIds, errors, 'MoviesOrSeriesIds must be an array');
+                if(errors.length > currentErrors) return;
+
+                moviesOrSeriesIds.forEach(movieOrSerieId => {
+                    validateType(movieOrSerieId, 'number', errors, 'Each Movie or Serie Id must be a number');
+                    if(errors.length > currentErrors) return;
+                    validateIsPositive(movieOrSerieId, errors, 'Each Movie or Serie Id must be a positive number');
+                    if(errors.length > currentErrors) return;
+                    validIsInteger(movieOrSerieId, errors, 'Each Movie or Serie Id must be an integer');
+                });
+            }
+
+            if(method === methods.Post) {
+                validation(moviesOrSeriesIds, errors);
+            } else if(method === methods.Put) {
+                const { add, remove } = moviesOrSeriesIds;
+                if(add?.length) validation(add, errors);
+                if(remove?.length) validation(remove, errors);
+            }
+        },
+        async (moviesOrSeriesIds, errors, method, characterId) => { 
+            const validateMoviesExists = async (moviesOrSeriesIds, errors) => {
+                const existingMoviesOrSeriesIds = await MovieOrSerie.findAll({ 
+                        where: { 
+                            id: { [Op.or] : moviesOrSeriesIds } 
+                        },
+                        attributes: [ 'id' ]
+                    })
+                    .then(moviesOrSeries => {
+                        return moviesOrSeries.map(movieOrSerie => movieOrSerie.dataValues.id);
+                    });
+
+                if(existingMoviesOrSeriesIds.length !== moviesOrSeriesIds.length) {
+                    const nonExistingMoviesOrSeriesIds = moviesOrSeriesIds.map(movieOrSerieId => {
+                            if(!existingMoviesOrSeriesIds.includes(movieOrSerieId)) {
+                                return movieOrSerieId;
+                            }
+                        })
+                        .filter(movieOrSerieId => movieOrSerieId !== null && movieOrSerieId !== undefined);
+
+                    errors.push(`The following Movies or Series Ids are wrong and do not reference a MovieOrSerie record: [ ${ nonExistingMoviesOrSeriesIds.join(', ') } ]`);
+                }
+            }
+
+            const validateMovieIsNotAlreadyRelated = async (moviesOrSeriesIds, errors, characterId) => {
+                const existingMoviesOrSeriesIds = await CharacterMovieOrSerie.findAll({
+                    where: { 
+                        [Op.or]: moviesOrSeriesIds.map(movieOrSerieId => ({
+                            movieOrSerieId,
+                            characterId
+                        }))
+                    }
+                })
+                .then(charactersMoviesOrSeries => {
+                    return charactersMoviesOrSeries.map(characterMovieOrSerie => 
+                        characterMovieOrSerie.dataValues.movieOrSerieId
+                    );
+                });
+
+                if(existingMoviesOrSeriesIds.length) {
+                    const alreadyExistingMoviesOrSeriesIds = moviesOrSeriesIds.map(movieOrSerieId => {
+                        if(existingMoviesOrSeriesIds.includes(movieOrSerieId)) {
+                            return movieOrSerieId;
+                        }
+                    })
+                    .filter(movieOrSerieId => movieOrSerieId !== null && movieOrSerieId !== undefined);
+
+                    errors.push(`The following MoviesOrSeries are already related to the Character: [ ${ alreadyExistingMoviesOrSeriesIds.join(', ') } ]`)
+                }
+            }
+
+            const validateMovieIsRelatedToCharacter = async (moviesOrSeriesIds, errors, characterId) => {
+                const existingMoviesOrSeriesIds = await CharacterMovieOrSerie.findAll({
+                        where: { 
+                            [Op.or]: moviesOrSeriesIds.map(movieOrSerieId => ({
+                                movieOrSerieId,
+                                characterId
+                            }))
+                        }
+                    })
+                    .then(charactersMoviesOrSeries => {
+                        return charactersMoviesOrSeries.map(characterMovieOrSerie => {
+                            characterMovieOrSerie.dataValues.movieOrSerieId
+                        });
+                    });
+
+                if(existingMoviesOrSeriesIds.length !== moviesOrSeriesIds.length) {
+                    const nonExistingMoviesOrSeriesIds = moviesOrSeriesIds.map(movieOrSerieId => {
+                        if(!existingMoviesOrSeriesIds.includes(movieOrSerieId)) {
+                            return movieOrSerieId;
+                        }
+                    })
+                    .filter(movieOrSerieId => movieOrSerieId !== null && movieOrSerieId !== undefined);
+
+                    errors.push(`The following MoviesOrSeries are not related to the Character: [ ${ nonExistingMoviesOrSeriesIds.join(', ') } ]`);
+                }
+            }
+
+            if(method === methods.Post) {
+                await validateMoviesExists(moviesOrSeriesIds, errors);
+            } else if(method === methods.Put) {
+                const { add , remove } = moviesOrSeriesIds;
+                if(add?.length) {
+                    const currentErrors = errors.length;
+                    await validateMoviesExists(add, errors);
+                    if(errors.length > currentErrors) return;
+                    await validateMovieIsNotAlreadyRelated(add, errors, characterId);
+                }
+                if(remove?.length) await validateMovieIsRelatedToCharacter(remove, errors, characterId);
+            }
+        },
     ]
 };
 
-module.exports = (req, res, next) => {
-    const { name, age, weight, history, image } = req.body;
+const postValidations = async (req, res, next) => {
+    const { name, age, weight, history, image, moviesOrSeriesIds } = req.body;
 
     const valuesByValidationsNames = {
-        name,
-        age,
-        weight,
-        history,
-        image
+        name: name,
+        age: age,
+        weight: weight,
+        history: history,
+        image: image
     };
 
+    if(moviesOrSeriesIds) valuesByValidationsNames.moviesOrSeriesIds = moviesOrSeriesIds;
+
     const errors = [];
-    console.log(Object.keys(validations));
-    Object.keys(validations).forEach(key => {
-        validations[key].forEach(validation => validation(valuesByValidationsNames[key], errors));
-    });
+    const objectKeys = Object.keys(valuesByValidationsNames);
+
+    for(let i = 0; i < objectKeys.length; i++) {
+        const key = objectKeys[i];
+        const fieldValidations = validations[key];
+
+        for(let j = 0; j < fieldValidations.length; j++) {
+            const value = valuesByValidationsNames[key];
+            await fieldValidations[j](value, errors, methods.Post);
+        }
+    }
+
     req.errors = errors;
 
     next();
+}
+
+const putValidations = async (req, res, next) => {
+    const { name, age, weight, history, image, moviesOrSeriesIds } = req.body;
+    const { characterId } = req.params;
+
+    const valuesByValidationsNames = {};
+    if(characterId) valuesByValidationsNames.characterId = characterId;
+    if(name) valuesByValidationsNames.name = name;
+    if(age) valuesByValidationsNames.age = age;
+    if(weight) valuesByValidationsNames.weight = weight;
+    if(history) valuesByValidationsNames.history = history;
+    if(image) valuesByValidationsNames.image = image;
+    if(moviesOrSeriesIds) valuesByValidationsNames.moviesOrSeriesIds = moviesOrSeriesIds;
+
+    const errors = [];
+    const objectKeys = Object.keys(valuesByValidationsNames);
+
+    for(let i = 0; i < objectKeys.length; i++) {
+        const key = objectKeys[i];
+        const fieldValidations = validations[key];
+
+        for(let j = 0; j < fieldValidations.length; j++) {
+            const value = valuesByValidationsNames[key];
+            await fieldValidations[j](value, errors, methods.Put, characterId);
+        }
+    }
+
+    req.errors = errors;
+
+    next();
+}
+
+const deleteValidations = async (req, res, next) => {
+    const { characterId } = req.params;
+
+    const errors = [];
+    await validations.characterId[0](characterId, errors, methods.Delete);
+    req.errors = errors;
+
+    next();
+}
+
+module.exports = {
+    postValidations,
+    putValidations,
+    deleteValidations
 };
--- routes/charactersRouter.js
+++ routes/charactersRouter.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 
-const { Character, MovieOrSerie } = require('../database');
+const { Op } = require('sequelize');
+
+const { Character, MovieOrSerie, CharacterMovieOrSerie } = require('../database');
 
 const { searchCharacter } = require('../helpers/searchHelper');
 
-const fieldsValidations = require('../middlewares/charactersValidator');
+const { postValidations, putValidations, deleteValidations } = require('../middlewares/charactersValidator');
 
 router.get('/', async (req, res) => {
     const characters = await Character.findAll({
@@ -38,38 +40,47 @@ router.get('/:characterId', async (req, res) => {
     return res.status(200).json({ result: 'success', character });
 });
 
-router.post('/', fieldsValidations, async (req, res) => {
+router.post('/', postValidations, async (req, res) => {
     const { errors } = req;
     if(errors.length) {
         return res.status(400).json({ result: 'error', errors });
     }
 
+    let character = {};
+    const { moviesOrSeriesIds } = req.body;
     try {
-        const character = await Character.create(req.body);
+        character = await Character.create(req.body);
+
+        if(moviesOrSeriesIds?.length) {
+            const { id } = character.dataValues;
+
+            await CharacterMovieOrSerie.bulkCreate(moviesOrSeriesIds.map(movieOrSerieId => ({
+                movieOrSerieId,
+                characterId: id
+            })));
+        }
     } catch(error) {
-        return res.status(500).json({ result: 'error', errors: [ error ]});
+        return res.status(500).json({ result: 'error', errors: [ error.message ]});
     }
+
+    character = await Character.findByPk(character.dataValues.id, {
+        include: {
+            model: MovieOrSerie,
+            attributes: []
+        }
+    });
 
     return res.status(201).json({ result: 'success', message: 'Character created succesfully', character });
 });
 
-router.put('/:characterId', fieldsValidations, async (req, res) => {
+router.put('/:characterId', putValidations, async (req, res) => {
     const { errors } = req;
     if(errors.length) {
         return res.status(400).json({ result: 'error', errors });
     }
 
+    const { name, age, weight, history, image, moviesOrSeriesIds } = req.body;
     const { characterId } = req.params;
-    if(!characterId) {
-        return res.status(400).json({ result: 'error', errors: [ `characterId not specified` ]});
-    }
-
-    let character = await Character.findByPk(characterId);
-    if(!character) {
-        return res.status(204).json({ result: 'error', errors: [ `No character found with id ${ characterId}` ]});
-    }
-
-    const { name, age, weight, history, image } = req.body;
 
     const updateFields = {};
     if(name) updateFields.name = name;
@@ -77,34 +88,58 @@ router.put('/:characterId', fieldsValidations, async (req, res) => {
     if(weight) updateFields.weight = weight;
     if(history) updateFields.history = history
     if(image) updateFields.image = image
-    
+
     try {
-        await character.update(updateFields);
-        await character.save();
+        await Character.update(updateFields, { where: { id: characterId }});
+   
+        if(moviesOrSeriesIds) {
+            const { add, remove } = moviesOrSeriesIds;
+
+            if(add?.length) {
+                await CharacterMovieOrSerie.bulkCreate(add.map(movieOrSerieId => ({
+                    movieOrSerieId,
+                    characterId
+                })));
+            }
+
+            if(remove?.length) {
+                await CharacterMovieOrSerie.destroy({
+                    where: { 
+                        [Op.or]: remove.map(movieOrSerieId => ({
+                            movieOrSerieId,
+                            characterId
+                        }))
+                    }
+                });
+            }
+        }
     } catch(error) {
-        return res.status(500).json({ result: 'error', errors: [ error ]});
+        return res.status(500).json({ result: 'error', errors: [ error.message ]});
     }
     
+    const character = await Character.findByPk(characterId, {
+        include: {
+            model: MovieOrSerie,
+            through: { attributes: [] }
+        }
+    });
+
     return res.status(201).json({ result: 'success', message: 'Character updated succesfully', character });
 });
 
-router.delete('/:characterId', async (req, res) => {
+router.delete('/:characterId', deleteValidations, async (req, res) => {
+    const { errors } = req;
+    if(errors.length) {
+        return res.status(400).json({ result: 'error', errors });
+    }
+
     const { characterId } = req.params;
-    if(!characterId) {
-        return res.status(400).json({ result: 'error', errors: [ `characterId not specified` ]});
-    }
-
-    let character = await Character.findByPk(characterId);
-    if(!character) {
-        return res.status(204).json({ result: 'error', errors: [ `No character found with id ${ characterId}` ]});
-    }
-
     try {
-        character = await Character.destroy({
+        await Character.destroy({
             where: { id: characterId }
         });
     } catch(error) {
-        return res.status(500).json({ result: 'error', errors: [ error ]});
+        return res.status(500).json({ result: 'error', errors: [ error.message ]});
     }
 
     return res.status(201).json({ result: 'success', message: 'Character deleted succesfully' });
